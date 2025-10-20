eaf491a9bef0ee7dc74445282bd66a2e368af4a2
maksimovtema501 <maksimovtema501@gmail.com>
2025-10-20 19:00:01 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+﻿const form = document.getElementById("tradingForm");
+
+// Вставь сюда URL твоего Google Apps Script веб-приложения
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzdoNhZXN9c4g1YZ_PL9qNahLrkzfTs2if3w2ab4F7bYBbPrQ9UFTM7qeScziA5oy2K/exec";
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const data = {
+    name: document.getElementById("name").value,
+    result: document.getElementById("result").value,
+    drawdown: document.getElementById("drawdown").value,
+    emotion: document.getElementById("emotion").value,
+    discipline: document.getElementById("discipline").value
+  };
+
+  try {
+    const response = await fetch(GOOGLE_SCRIPT_URL, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data)
+    });
+
+    const res = await response.json();
+
+    if(res.status === "ok") {
+      alert("Данные сохранены!");
+      form.reset();
+    } else {
+      alert("Ошибка: " + res.message);
+    }
+  } catch (err) {
+    alert("Ошибка сети или сервера: " + err);
+  }
+});
